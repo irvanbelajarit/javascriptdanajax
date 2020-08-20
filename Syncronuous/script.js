9dd12e59865a5d67aeb1bdb3f3308af66d735fb3
irvanln <irvina22@gmail.com>
2020-08-21 01:23:27 +0700

    syncronuous ajax

---
+++ Syncronuous/script.js
@@ -0,0 +1,17 @@
+for(var i = 0;i<100;i++){
+    var xmlHTTP;
+    xmlHTTP = new XMLHttpRequest();
+  
+    xmlHTTP.open('GET','data.txt',false);
+    xmlHTTP.send();
+
+    if (xmlHTTP.status === 200){
+    console.log(xmlHTTP);
+    document.writeln(xmlHTTP.responseText);
+    }
+
+
+}
+   
+
+
