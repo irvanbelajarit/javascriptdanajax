420a21c9c48e2cf09f24c0d679e43caef4651a6b
irvanln <irvina22@gmail.com>
2020-08-23 23:17:22 +0700

    event ajax 2

---
+++ eventajax/script.js
@@ -0,0 +1,28 @@
+
+  
+    var xmlHTTP;
+    xmlHTTP = new XMLHttpRequest();
+  
+    xmlHTTP.open('GET','data.json');
+    xmlHTTP.onreadystatechange = function(){
+        if ((xmlHTTP.readyState === 4) && (xmlHTTP.status === 200)){
+
+          var items = JSON.parse(xmlHTTP.responseText);
+         
+          var output = '<ul>';
+          for (var key in items){
+            output += '<li>' + items[key].nama + '</li>';
+          }
+
+          output +='</ul>';
+           document.getElementById('update').innerHTML = output;
+            }
+
+    }
+    xmlHTTP.send();
+
+    
+
+
+
+
